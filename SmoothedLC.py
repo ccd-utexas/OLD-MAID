3a525cac55e3724654b27692db870536986fcab6
Keaton Bell <keatonb@astro.as.utexas.edu>
2016-02-04 17:24:05 -0600

    Smoothed light curve has vertical marker for displayed frame

--- SmoothedLC.py
+++ SmoothedLC.py
@@ -26,9 +26,9 @@ class KernelDialog(QtGui.QDialog):
         typeLabel.setBuddy(self.typeEdit)
         widthLabel = QtGui.QLabel("Kernel &width")
         self.widthEdit = QtGui.QSpinBox()
-        self.widthEdit.setMinimum(2)
-        self.widthEdit.setMaximum(200)
-        #self.widthEdit.setValue(currentwidth)
+        self.widthEdit.setMinimum(3)
+        self.widthEdit.setMaximum(201)
+        #self.widthEdit.setValue(width)
         widthLabel.setBuddy(self.widthEdit)
         self.buttons = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel,
                                         QtCore.Qt.Horizontal, self)
@@ -51,7 +51,7 @@ class KernelDialog(QtGui.QDialog):
         
     @staticmethod
     def getKernelFormat(parent = None):
-        dialog = KernelDialog(parent)
+        dialog = KernelDialog(parent,width=width)
         result = dialog.exec_()
         kerneltype,width = dialog.kernelFormat()
         return (kerneltype,width, result == QtGui.QDialog.Accepted)
@@ -60,53 +60,90 @@ class KernelDialog(QtGui.QDialog):
 #set up a class that holds all the smoothing kernel information
 class smoothingkernel:
     """Holds all smoothing kernel info"""
-    kerneltype = 0
-    width = 11 #points
-    kernel=[]
-    types = ['Uniform','Epanechnikov']
+    def __init__(self):
+        self.kerneltype = 0
+        self.width = 11 #points
+        self.kernel=[]
+        self.types = ['Uniform','Epanechnikov']
+        self.setkernel(0,11)
     def setkernel(self,kerneltype,width):
         if kerneltype == 1: #Epanechnikov
             u=(2.*np.arange(width)/(float(width)-1.))-0.5
             self.kernel = 0.75*(1.-u**2.)
             self.kernel /= np.sum(self.kernel)
-            #processLog.log("Using "+self.types[kerneltype]+" smoothing kernel of width "+str(width))
+            self.log("Using "+self.types[kerneltype]+" smoothing kernel of width "+str(width))
         elif kerneltype == 0: #Uniform
             self.kernel = np.ones(width)/float(width)
             #processLog.log("Using "+self.types[kerneltype]+" smoothing kernel of width "+str(width))
     def openKernelDialog(self):
             dkerneltype,dwidth,daccepted = KernelDialog.getKernelFormat()
-            if daccepted and (dkerneltype in range(len(types))) and (dwidth > 1): 
+            if daccepted and (dkerneltype in range(len(self.types))) and (dwidth > 1): 
                 self.setkernel(dkerneltype,dwidth)
-    def __init__(self):
-        self.setkernel(0,11)
+    def log(self,text,level=0):
+        self.emit(QtCore.SIGNAL("log"),text,level)
 
 
 class SmoothedLC(pg.PlotWidget):
     
     def __init__(self):
         super(SmoothedLC,self).__init__()
-        #self.lcline = pg.PlotCurveItem()
+        #Vertical line shows currently displayed point
+        self.currentind = pg.InfiniteLine(pen='y')
+        self.addItem(self.currentind)
+        #Scatter plot shows light curve
         self.lcscatter = self.plot(brush=(255,0,0), pen='w',symbol='o') #Scatter plot
         self.setTitle("Smoothed Light Curve")
         self.setLabel('left', 'smoothed flux')
         self.setLabel('bottom', 'time (s)')
         
+        self.numpts=0
+        
         self.kernel = smoothingkernel()
+        #self.connect(self.kernel,QtCore.SIGNAL("log"),self.log)
+                       
+        #right click action
+        #disable the normal context menu
+        self.setMenuEnabled(False)
+        self.scene().sigMouseClicked.connect(self.rightclicked)
 
 
     def plotdata(self,time,flux,exptime):
         '''
         Smooth and plot light curves.
         '''
+        self.exptime = exptime
         #only update if there are enough points:
-        if len(flux) > self.kernel.width:        
+        if len(flux) > self.kernel.width: 
             #Fill bad points with nans:
-            alltimes=np.arange(min(time),max(time)+exptime,exptime)/exptime
+            alltimes=np.arange(min(time),max(time)+exptime,exptime)
             filledflux = np.empty(alltimes.shape)
             filledflux[:] = np.NAN
-            filledflux[(time/exptime).astype(int)] = flux
+            filledflux[(((time-min(time)))/exptime).astype(int)] = flux
             #print type(filledflux)
             
             #convolve with smoothing kernel
             fluxsmoothed=convolve(filledflux,self.kernel.kernel,boundary='extend')
             self.lcscatter.setData(alltimes,fluxsmoothed)
+            
+            #Update vertical line marker if currently located at end of lc
+            if self.currentind.value() >= self.numpts*self.exptime - self.exptime:
+                self.currentind.setValue(max(alltimes))
+            
+            #update numpts
+            self.numpts = max(alltimes)/exptime
+            
+    #Inspect location where right clicked
+    def rightclicked(self,event):
+        if event.button() == 2: #right click
+            event.accept()
+            x=self.lcscatter.getViewBox().mapSceneToView(event.scenePos()).x()
+            ind = np.round(x/self.exptime)
+            if ind > self.numpts: #Follow newest frames
+                ind = self.numpts
+            elif ind < 0: ind = 0
+            self.currentind.setValue(ind*self.exptime)
+            #Emit a signal so other views can sync up with this            
+            #self.emit(QtCore.SIGNAL("inspectind"),ind)
+        
+    def log(self,text,level=0):
+        self.emit(QtCore.SIGNAL("log"),text,level)
