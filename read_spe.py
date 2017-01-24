c48da83cf7019239b15d4e49285aceedeb51b398
Keaton Bell <keatonb@astro.as.utexas.edu>
2017-01-24 04:09:23 -0600

    add license to header

--- read_spe.py
+++ read_spe.py
@@ -9,6 +9,29 @@ For the XML footer, see
 ftp://ftp.princetoninstruments.com/public/Manuals/Princeton%20Instruments/Experiment%20XML%20Specification.pdf
 
 Note: Use with SPE 3.0. Not backwards compatible with SPE 2.X.
+
+The MIT License (MIT)
+
+Copyright (c) 2014 ccd-admin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 """
 # TODO: make pytest modules with test_yes/no_footer.spe files
 # TODO: fix docstrings to match numpy style: https://github.com/numpy/numpy/blob/master/doc/example.py
